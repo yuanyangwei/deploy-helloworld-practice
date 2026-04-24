1fbaef9b9366bd96bc38b4fdb9b296e4b370d3fe
yuanyangwei <yuanyang.wei@gmail.com>
2026-04-24 23:43:20 +0800

    add configuration for vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "yuanyang-terraform-state-2026" 
     key    = "dev/terraform.tfstate"
-    region = "ap-southeast-1"
+    region = var.aws_region
     dynamodb_table = "terraform-state-lock-wei"
   }
 }
@@ -18,15 +18,48 @@ provider "aws" {
   region = var.aws_region
 }
 
-# --- NETWORK DATA ---
-data "aws_vpc" "default" { default = true }
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+# --- VPC & NETWORK DATA ---
+data "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.project_name}-vpc"
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = data.aws_vpc.main.id
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+resource "aws_subnets" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone = "${var.aws_region}a"
+  tags = {
+    name = "${var.project_name}-public-subnet"
+  }
+}
+
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = { Name = "${var.project_name}-public-rt" }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
 # --- REGISTRY ---
 resource "aws_ecr_repository" "repo" {
   name         = var.project_name
@@ -36,6 +69,7 @@ resource "aws_ecr_repository" "repo" {
 # --- SECURITY ---
 resource "aws_security_group" "ecs_sg" {
   name   = "${var.project_name}-sg"
+  description = "Allow inbound traffic to ECS task"
   vpc_id = data.aws_vpc.default.id
 
   ingress {
@@ -92,12 +126,16 @@ resource "aws_ecs_service" "main" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = data.aws_subnets.default.ids
+    subnets          = [aws_subnet.public.id]
     assign_public_ip = true
     security_groups  = [aws_security_group.ecs_sg.id]
   }
 }
 
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name              = "/ecs/${var.project_name}"
+  retention_in_days = 7
+}
 # --- PERMISSIONS ---
 resource "aws_iam_role" "ecs_exec_role" {
   name = "${var.project_name}-exec-role"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,5 @@ variable "project_name" { default = "auto-deployment-practice" }
 variable "container_port" { default = 5000 }
 variable "cpu_units" { default = "256" }
 variable "memory_units" { default = "512" }
+variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "public_subnet_cidr" {default ="10.0.1.0/24"}
